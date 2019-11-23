49a4d20a76382324f4373437963d78eda926a6b3
Jackie Christensen <jackie@e-komplet.dk>
2019-11-23 16:26:52 +0100

    Coins and others

---
+++ Assets/Scripts/ThrowCoins.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCoins : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _coin;
+    [SerializeField]
+    float _amount;
+
+    public void ThrowSomeCoins()
+    {
+
+    }
+}
