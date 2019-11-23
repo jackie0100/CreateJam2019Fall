25d5ef76c0781fc1804234a0d90b5a3504266bfd
Jackie Christensen <jackie@e-komplet.dk>
2019-11-23 18:28:51 +0100

    Magnet pickup and coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
